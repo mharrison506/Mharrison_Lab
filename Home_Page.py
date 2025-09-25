f4699999e8f689ee15ca979bc7ea34f6edac3b5e
mharrison506 <fischerharrison16@gmail.com>
2025-09-25 18:04:11 -0400

    Add files via upload

---
+++ Home_Page.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+# Title of App
+st.title("Web Development Lab01")
+
+# Assignment Data 
+# TODO: Fill out your team number, section, and team members
+
+st.header("CS 1301")
+st.subheader("Web Development - Section B")
+st.subheader("Fischer Harrison")
+
+
+# Introduction
+# TODO: Write a quick description for all of your pages in this lab below, in the form:
+#       1. **Page Name**: Description
+#       2. **Page Name**: Description
+#       3. **Page Name**: Description
+#       4. **Page Name**: Description
+
+st.write("""
+Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are
+1. **Data About Me🧍🧍🧍**: Just a quick little summary of what I like to do and where I am from
+2. **Quizzzz🎉🎉🎉**: Just a fun little quiz to see how crazy you actually are from a score of 0-5 with 5 being the best score
+
+
+""")
+
---
+++ info.py
@@ -0,0 +1,85 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/king.jpg"
+about_me = "I am Fischer Harrison and I am a second year student at Georgia Tech studying Economics. After graduation I hope to become a consultant or anything to do with policy research (In the pic, that is Haynes King)"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "www.linkedin.com/in/fischer-harrison-b04574366"
+my_github_url = "https://github.com/mharrison505"
+my_email_address = "fischerharrison16@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Economics',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Spring 2028',
+    'GPA': '3.45'
+}
+course_data = {
+    "code":["CS 1301", "Econ 2250", "MATH 1552"], 
+    "names":["Intro to CS", "Statistics for Economics", "Calc 2"], 
+    "semester_taken":["First", "First", "First"],
+    "skills":["Learned to be the basic concepts of pythong", "Learned Basics function in R and how we can use statistic to predict outcomes", "Learned area under a curve and different techniques to get the area."],
+    }
+experience_data = {
+    "Host at Metro Diner": (["- Sat and Trained new host",
+                                                                          "- Devoloped a system that decreased wait times", "- Made my Boss Proud"],"Images/metro.jpg"),
+    "Referee at Hoppers Painball":(["- Managed man babbies",
+                                                           "- Made sure everyone was safe and playing by the rules"],"Images/paint.jpg"),
+
+}
+
+projects_data = {
+    "NFL Predictor": "Created a table that added numerical value to 50+ stats for each team and developed a point system in excel.  It had a 70-80% win rate",
+}
+
+programming_data = {
+    "Python": 50,
+    "R": 10,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "R": "☕",
+    "C": "🔍",
+}
+spoken_icons = { "English": "us",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English 🦅": "Fluent",
+    "Spanish 🇪🇸": "proficient",
+}
+leadership_data = {
+    "Lead host/Referee": (["- Was basically the manager of a small team"],"Images/host.jpg"),
+
+}
+activity_data={
+    "Second Harvest Food Bank": ["- Packaged around 15,000 student lunches over the summer with a team of around 20", 
+            "- Sorted expired food"]
+
+}
+
+school_data = {
+    "Econ Club": [
+        "- Held discussions about current economic trends",
+        "- Organized research panels to see what professors and students are researching"
+    ],
+    "Investment Committee🤑🤑🤑🤑": [
+        "- Learned basic accounting skills, and how to make a DCF model",
+        "- Final Project consisted of making a stock pitch."
+    ]
+}
